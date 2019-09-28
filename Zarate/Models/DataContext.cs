09415b157c193a74578abb701642f11375405721
Bruno <brunoivanzaratefernandez@gmail.com>
2019-09-28 09:30:04 -0400

    Conexion con base de datos

---
+++ Zarate/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Zarate.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Zarate/Models/Fairy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zarate.Models
+{
+    public enum wishes
+    {
+        Balones,
+        Bocinas,
+        Barco,
+    }
+    public class Fairy
+    {
+        [Key]
+        public int FairyID { get; set; }
+        [Required(ErrorMessage ="Nombre completo")]
+        public string NickName { get; set; }
+        [Required]
+        public wishes Wishes { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+    }
+}
